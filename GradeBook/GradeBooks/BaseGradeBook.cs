fd2f24873c95e2632423ee13a009f822114c7dbc
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-10-14 23:25:43 +0200

    Test/Task .2.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -19,6 +19,8 @@ namespace GradeBook.GradeBooks
             Name = name;
             Students = new List<Student>();
         }
+        
+        public GradeBookType Type { get; set; }
 
         public void AddStudent(Student student)
         {
