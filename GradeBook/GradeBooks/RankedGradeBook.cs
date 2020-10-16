1b89ce62b94c6abab4e3a9f0b1699319f0224164
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-10-16 22:10:31 +0200

    Tests 18/ Task .10.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
@@ -32,6 +33,45 @@ namespace GradeBook.GradeBooks
                 return;
             }
             base.CalculateStudentStatistics(name);
+        }
+        
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            int highAvStudents =
+                Students
+                .Select(a => a)
+                .Where(a => a.AverageGrade >= averageGrade)
+                .Count();
+            var highAvStudentsPercentage = Decimal.Divide(highAvStudents, Students.Count) * 100;
+            var percentToGrade = (100 - highAvStudentsPercentage);
+            switch (percentToGrade)
+            {
+                case decimal percentage when (percentToGrade >= 80):
+                    return 'A';
+                case decimal percentage when (percentToGrade < 80 && percentToGrade >= 60):
+                    return 'B';
+                case decimal percentage when (percentToGrade < 60 && percentToGrade >= 40):
+                    return 'C';
+                case decimal percentage when (percentToGrade < 40 && percentToGrade >= 20):
+                    return 'D';
+                default:
+                    return 'F';
+            }
         }
 
     }
