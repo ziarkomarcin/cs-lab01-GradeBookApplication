9549fe441fa408e98e5d2c7ea83e6114c512db70
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-10-16 22:22:00 +0200

    Tests 20

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
@@ -7,7 +7,7 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook:BaseGradeBook
     {
-        public StandardGradeBook(string name):base(name)
+        public StandardGradeBook(string name, bool isWeighted):base(name, isWeighted)
         {
             Type = GradeBookType.Standard;
         }
